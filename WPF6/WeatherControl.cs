71d5204174e0da34c3061c17117c4a298f3a2293
Вадим Семочкин <vadimsemochkin96@yandex.ru>
2022-01-16 12:59:02 +0300

    Добавьте файлы проекта.

---
+++ WPF6/WeatherControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF6
+{
+    class WeatherControl: DependencyObject
+    {
+       
+
+        public static readonly DependencyProperty TempProperty = DependencyProperty.Register(
+           nameof(Temp),
+           typeof(int),
+           typeof(WeatherControl),
+           new FrameworkPropertyMetadata(
+               0,
+               FrameworkPropertyMetadataOptions.AffectsMeasure |
+               FrameworkPropertyMetadataOptions.AffectsRender,
+               null,
+               new CoerceValueCallback(CoerceTemp)),
+           new ValidateValueCallback(ValidateTemp));
+
+        public string windDirection;
+        public int windSpeed;
+
+        private static bool ValidateTemp(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+                return true;
+            else
+                return false;
+        }
+
+        private static object CoerceTemp(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+                return v;
+            else
+                return 0;
+        }
+
+        public int Temp
+        {
+            get => (int)GetValue(TempProperty);
+            set => SetValue(TempProperty, value);
+        }
+        
+        public enum Osadki
+        {
+            солнечно = 0,
+            облачно = 1,
+            дождь = 2,
+            снег = 4
+        }
+    }
+}
